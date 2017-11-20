b6191dd2a280f8d620441fcc96337363477316ae
Hari <rhari26@gmail.com>
2017-11-20 23:05:07 +0530

    comment changes

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,2 +1,34 @@
 class CommentsController < ApplicationController
+ before_action :set_post, only: [:create]
+ before_action :set_comment, only: [:edit, :update, :destroy]
+
+ def new
+ end
+
+ def create
+  @comment = Comment.new(comment: params[:text], post_id: @post.id, user_id: current_user.id)
+  @comment.save
+ end
+
+ def edit
+ end
+
+ def update
+  @comment.update comment: params[:text]
+ end
+
+ def destroy
+ end
+
+ private
+
+ def set_comment
+  @comment = Comment.find_by(id: params[:id])
+  redirect_to page_not_found unless @comment.present?
+ end
+
+ def set_post
+  @post = Post.find_by(id: params[:post_id])
+  redirect_to page_not_found unless @post.present?
+ end
 end
